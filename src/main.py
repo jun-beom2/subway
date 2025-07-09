941c44cec854140ac6139746a7f39c8b49dbfe88
JY <chibulls23@naver.com>
2025-07-09 15:00:37 +0900

    없는 역 예외처리
    간단하게만 처리함

--- src/main.py
+++ src/main.py
@@ -16,8 +16,6 @@ ex_destin = "칠성시장"
 temp = ld.DfLine1_upper.loc[0]
 
 
-#print(temp.iloc[0])
-
 ListLine1  = []
 ListLine2 = []
 ListLine3 = [] 
@@ -32,19 +30,19 @@ for x in ld.DfLine2_lower["역명"] :
 for x in ld.DfLine3_lower["역명"] :
     ListLine3.append(x)
 
-if ex_depart in ListLine1 :
-    print("1호선")
-    print(ListLine1.index(ex_depart))
-if ex_depart in ListLine2 :
-    print("2호선")
-    print(ListLine2.index(ex_depart))
-if ex_depart in ListLine3 :
-    print("3호선")
-    print(ListLine3.index(ex_depart))
+#if ex_depart in ListLine1 :
+#    print("1호선")
+#    print(ListLine1.index(ex_depart))
+#if ex_depart in ListLine2 :
+#    print("2호선")
+#    print(ListLine2.index(ex_depart))
+#if ex_depart in ListLine3 :
+#    print("3호선")
+#    print(ListLine3.index(ex_depart))
 
-for x in ld.DfLine1_lower["환승"] :
-    if pd.notna(x):
-        print(x)
+#for x in ld.DfLine1_lower["환승"] :
+#    if pd.notna(x):
+#        print(x)
 
 def transfer(routes) :
     transroutes = []
@@ -56,16 +54,22 @@ def transfer(routes) :
 
 
 StrDepart = "다사" # input으로 입력받아서 할 예정
-StrDestin = "동대구역" # input으로 입력받아서 할 예정
+StrDestin = "서문시장" # input으로 입력받아서 할 예정
+IntDepartLine = 0
+IntDestinLine = 0
 DtCurrenttime = datetime.now() #현재 시간 불러오기
 
-distance, prev = di.dijkstra(ld.DictInterval_data,StrDepart)
-m_routes = di.routes(prev,StrDestin)
-arrivaltime = DtCurrenttime + timedelta(minutes=distance[StrDestin])
+try :
+    distance, prev = di.dijkstra(ld.DictInterval_data,StrDepart)
+    m_routes = di.routes(prev,StrDestin)
+    arrivaltime = DtCurrenttime + timedelta(minutes=distance[StrDestin])
+    print("현재시각", DtCurrenttime.time().strftime("%H:%M"))
+    print("최단시간", distance[StrDestin])
+    print("도착시각", arrivaltime.strftime("%H:%M"))
+    print(m_routes)
+except KeyError as e:
+    print("역명을 확인해주세요",e)
 
 
-print("현재시각", DtCurrenttime.time().strftime("%H:%M"))
-print("최단시간", distance[StrDestin])
-print("도착시각", arrivaltime.strftime("%H:%M"))
-print(m_routes)
+
 
